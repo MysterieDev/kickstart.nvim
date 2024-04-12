20916b6ae948782c5331ac760afb24c504f57f48
slezakIT <apps@slezak-it.de>
2024-04-12 22:00:56 +0200

    enable Zoom in neovide

--- init.lua
+++ init.lua
@@ -154,6 +154,42 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- [[ Enable Zoom with Neovide]]
+vim.g.gui_font_default_size = 12
+vim.g.gui_font_size = vim.g.gui_font_default_size
+vim.g.gui_font_face = 'JetBrains Mono'
+
+RefreshGuiFont = function()
+  vim.opt.guifont = string.format('%s:h%s', vim.g.gui_font_face, vim.g.gui_font_size)
+end
+
+ResizeGuiFont = function(delta)
+  vim.g.gui_font_size = vim.g.gui_font_size + delta
+  RefreshGuiFont()
+end
+
+ResetGuiFont = function()
+  vim.g.gui_font_size = vim.g.gui_font_default_size
+  RefreshGuiFont()
+end
+
+-- Call function on startup to set default value
+ResetGuiFont()
+
+-- Keymaps
+
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set({ 'n', 'i' }, '<C-+>', function()
+  ResizeGuiFont(1)
+end, opts)
+vim.keymap.set({ 'n', 'i' }, '<C-->', function()
+  ResizeGuiFont(-1)
+end, opts)
+vim.keymap.set({ 'n', 'i' }, '<C-BS>', function()
+  ResetGuiFont()
+end, opts) -- Keymaps
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
